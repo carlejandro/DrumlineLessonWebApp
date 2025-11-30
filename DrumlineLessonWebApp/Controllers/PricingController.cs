8a3ba8a6c2cbba4ac03e302d938efdb1e13b751f
ALEX SUAREZ-MARTINEZ <60011961@sfcollege.edu>
2025-11-30 14:49:15 -0500

    Added the Home Page, and Pricing Page views with CSS. A controller and Model were also added for Pricing Options.

---
+++ DrumlineLessonWebApp/Controllers/PricingController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using DrumlineLessonWebApp.Models;
+using System.Collections.Generic;
+
+namespace DrumlineLessonWebApp.Controllers
+{
+    public class PricingController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            var plans = new List<PricingOption>
+{
+                new PricingOption
+                {
+                    Title = "1 Hour",
+                    Description = "Any Skill Level One hour with Me!",
+                    Price = 29.99m,
+                    Features = "1-Hour of Instruction on Any marching instrument",
+                    IsPopular = false
+                },
+
+                new PricingOption
+                {
+                    Title = "30 Minutes",
+                    Description = "For drummers looking to fit training in on a tight schedule.",
+                    Price = 15.99m,
+                    Features = "30 Minutes of instruction on Any Marching Instrument",
+                    IsPopular = true
+                }
+            };
+            return View(plans);
+        }
+    }
+}
---
+++ DrumlineLessonWebApp/Models/PricingOption.cs
@@ -0,0 +1,14 @@
+﻿namespace DrumlineLessonWebApp.Models
+{
+
+    public class PricingOption
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public string Features { get; set; }
+        public bool IsPopular { get; set; }
+
+    }
+
+}
